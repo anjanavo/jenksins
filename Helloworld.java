baf9d82770872b6bd198336679de6accb33bab07
anjanavo <151794647+anjanavo@users.noreply.github.com>
2023-11-22 20:05:09 -0600

    Delete Helloworld.java

--- Helloworld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hi, I am Anjana");
-    System.out.println("1002157096");
-    System.out.println("Jenkins");
-  }
-}
